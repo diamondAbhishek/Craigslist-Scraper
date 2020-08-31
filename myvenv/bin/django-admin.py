d0f4d9dc6c42d76abad23c8e12433d0353e687d4
Abhishek <singh.abhishek1996@gmail.com>
2020-08-31 20:04:29 +0530

    first commit

---
+++ myvenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/abhishek/Documents/Projects/Craigslist_Scraper/myvenv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
